4a42caf8d6f4528bfd21dd638d81a2b6d1518143
LitGithub <57814238+LitGithub@users.noreply.github.com>
2022-11-09 19:18:35 -0700

    Update main.py
    i am confused on why this code was so bad previously

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pygame #handles graphics, sound, game timings, keyboard input, etc
 import random
 pygame.init()
@@ -13,29 +12,25 @@ running = True
 list = []
 amount = 50
 for x in range(0, amount):
-    list.append(random.randrange(10, 500))
+    list.append(random.randrange(0, 500))
     
 def draw():
     screen.fill((0, 0, 0))
-    i = 0
-    for x in list:
-        pygame.draw.rect(screen, (255, 255, 255), (((width / amount) * i), 0, (width / amount), x))
-        i+=1
+    for x in range(len(list)):
+        pygame.draw.rect(screen, (255, 255, 255), (((width / amount) * x), 500-list[x], (width / amount), list[x]))
     pygame.display.flip() #update graphics
+
 while running: #sortof a game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    for i in range(len(list)):
-        for j in range(len(list)):
-            if i < 49:
-                j = i + 1
-            draw()
-            if(list[i] < list[j]):
-                #print(i, j)
-                list[i], list[j] = list[j], list[i]
-            pygame.draw.rect(screen, (255, 0, 0), (((width / amount) * i), 0, (width / amount), list[i]))
-            pygame.draw.rect(screen, (255, 0, 0), (((width / amount) * j), 0, (width / amount), list[j]))
-            pygame.display.flip()
+    for i in range(1, len(list)):
+        j = i - 1
+        draw()
+        if(list[i] < list[j]):
+            list[i], list[j] = list[j], list[i]
+        pygame.draw.rect(screen, (255, 0, 0), (((width / amount) * i), 500-list[i], (width / amount), list[i]))
+        pygame.draw.rect(screen, (255, 0, 0), (((width / amount) * j), 500-list[j], (width / amount), list[j]))
+        pygame.display.flip()
             
 pygame.quit()
